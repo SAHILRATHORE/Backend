4e32ee983dae84b17547c800e446f859e2137ae4
Sahil Rathore <sahilrathore605@gmail.com>
2025-12-30 14:58:16 +0530

    Fix all register controller bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         // file has been uploaded successfully
-        console.log("fie is uploded on cloudinary , response :", response.url);
+        // console.log("fie is uploded on cloudinary , response :", response.url);
+        fs.unlinkSync(localFilePath)
         return response
 
     } catch (error) {
